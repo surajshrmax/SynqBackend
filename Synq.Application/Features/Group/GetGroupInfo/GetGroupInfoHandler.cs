c3322d5596e4a241959d216234ae928bf0399b01
surajshrmax <suraj.shrmax@gmail.com>
2026-05-17 20:41:18 +0530

    perf(get-group-info): add redis cache layer

--- Synq.Application/Features/Group/GetGroupInfo/GetGroupInfoHandler.cs
+++ Synq.Application/Features/Group/GetGroupInfo/GetGroupInfoHandler.cs
@@ -2,29 +2,51 @@
 using Microsoft.EntityFrameworkCore;
 using Synq.Application.Common.Interfaces;
 using Synq.Application.DTOs;
-using Synq.Application.Mappers;
 
 namespace Synq.Application.Features.Group.GetGroupInfo;
 
-public class GetGroupInfoHandler(IApplicationDbContext dbContext, ICurrentUserService currentUserService) : IRequestHandler<GetGroupInfoQuery, GroupDto>
+public class GetGroupInfoHandler(
+    IApplicationDbContext dbContext,
+    ICurrentUserService currentUserService,
+    ICacheService cacheService,
+    IJsonHelper<GroupDto> jsonHelper) : IRequestHandler<GetGroupInfoQuery, GroupDto>
 {
-    public async Task<GroupDto> Handle(GetGroupInfoQuery query, CancellationToken cancellationToken)
-    {
-        var isValidRequest = await dbContext.ChatMembers.AsNoTracking().Where(cm => cm.ChatId == Guid.Parse(query.Id) && cm.UserId == currentUserService.UserId).FirstOrDefaultAsync(cancellationToken);
-        if (isValidRequest == null)
-        {
-            throw new UnauthorizedAccessException("You're not part of the group");
-        }
+  public async Task<GroupDto> Handle(GetGroupInfoQuery query, CancellationToken cancellationToken)
+  {
+    string key = $"group-info {query.Id}";
 
-        var chat = await dbContext.Chats.AsNoTracking().Where(c => c.Id == Guid.Parse(query.Id))
-            .Select(c => new GroupDto
-            {
-                Id = c.Id,
-                Title = c.Title,
-                ImageUrl = "",
-                MembersCount = c.ChatMembers.Count
-            }).FirstAsync(cancellationToken);
-        
-        return chat;
+    string cachedGroup = await cacheService.GetValueAsync(key);
+
+    if (cachedGroup != null)
+    {
+      return jsonHelper.Decode(cachedGroup);
     }
+
+    var isValidRequest = await dbContext.ChatMembers
+        .AsNoTracking()
+        .Where(cm => cm.ChatId == Guid.Parse(query.Id) &&
+            cm.UserId == currentUserService.UserId)
+        .FirstOrDefaultAsync(cancellationToken);
+
+    if (isValidRequest == null)
+    {
+      throw new UnauthorizedAccessException("You're not part of the group");
+    }
+
+    var chat = await dbContext.Chats.AsNoTracking().Where(c => c.Id == Guid.Parse(query.Id))
+        .Select(c => new GroupDto
+        {
+          Id = c.Id,
+          Title = c.Title,
+          ImageUrl = "",
+          MembersCount = c.ChatMembers.Count
+        }).FirstAsync(cancellationToken);
+
+    if (chat != null)
+    {
+      await cacheService.SetValueAsync(key, jsonHelper.Encode(chat), TimeSpan.FromMinutes(10));
+    }
+
+    return chat;
+  }
 }

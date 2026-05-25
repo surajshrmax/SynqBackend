a25b466834e59cf96c4e5d1d5718ba8ac1bc5fe3
surajshrmax <suraj.shrmax@gmail.com>
2026-05-25 19:29:31 +0530

    perf(update-message & delete-message): add redis caching

--- Synq.Application/Features/Message/DeleteMessage/DeleteMessageHandler.cs
+++ Synq.Application/Features/Message/DeleteMessage/DeleteMessageHandler.cs
@@ -1,14 +1,16 @@
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Synq.Application.Common.Interfaces;
+using Synq.Domain.Entities;
 
 namespace Synq.Application.Features.Message.DeleteMessage;
 
 public class DeleteMessageHandler(
     IApplicationDbContext dbContext,
     ICurrentUserService currentUserService,
-    IConnectionStore connectionStore,
-    IRealTimeMessageNotifier messageNotifier
+    IRealTimeMessageNotifier messageNotifier,
+    ICacheService cacheService,
+    IJsonHelper<Chat> jsonHelper
     ) : IRequestHandler<DeleteMessageCommand>
 {
   public async Task Handle(DeleteMessageCommand command, CancellationToken cancellationToken)
@@ -25,17 +27,36 @@ public class DeleteMessageHandler(
     dbContext.Messages.Remove(message);
     await dbContext.SaveChangesAsync(cancellationToken);
 
-    var chat = await dbContext.Chats.Where(c => c.Id == message.ChatId).Include(c => c.ChatMembers).FirstOrDefaultAsync(cancellationToken);
+    Chat? chat;
+    string chatKey = $"chat {message.ChatId}";
+
+    var cachedChat = await cacheService.GetValueAsync(chatKey);
+
+    if (cachedChat != null)
+    {
+      chat = jsonHelper.Decode(cachedChat);
+    }
+    else
+    {
+      chat = await dbContext.Chats
+        .AsNoTracking()
+        .Where(c => c.Id == message.ChatId)
+        .Select(c => new Chat
+        {
+          Id = c.Id,
+          ChatMembers = c.ChatMembers.Select(cm => new ChatMember
+          {
+            UserId = cm.UserId
+          }).ToList()
+        })
+        .FirstOrDefaultAsync(cancellationToken);
+
+      await cacheService.SetValueAsync(chatKey, jsonHelper.Encode(chat), TimeSpan.FromMinutes(30));
+    }
+
     var recieverId = chat.ChatMembers.FirstOrDefault(cm => cm.UserId != currentUserService.UserId);
 
-    if (recieverId != null && connectionStore.TryGet(recieverId.ToString()!, out string recieverConnectionId))
-    {
-      await messageNotifier.SendToUserAsync(recieverConnectionId, "MessageDeleted", messageId);
-    }
-
-    if (connectionStore.TryGet(currentUserService.UserId.ToString(), out string senderConnectionId))
-    {
-      await messageNotifier.SendToUserAsync(senderConnectionId, "MessageDeleted", messageId);
-    }
+    await messageNotifier.SendToUserAsync(recieverId.ToString(), "MessageDeleted", messageId);
+    await messageNotifier.SendToUserAsync(currentUserService.UserId.ToString(), "MessageDeleted", messageId);
   }
 }
--- Synq.Application/Features/Message/UpdateMessage/UpdateMessageHandler.cs
+++ Synq.Application/Features/Message/UpdateMessage/UpdateMessageHandler.cs
@@ -1,21 +1,23 @@
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Synq.Application.Common.Interfaces;
-using Synq.Application.DTOs;
 using Synq.Application.Mappers;
+using Synq.Domain.Entities;
 
 namespace Synq.Application.Features.Message.UpdateMessage;
 
 public class UpdateMessageHandler(
     IApplicationDbContext dbContext,
     ICurrentUserService currentUserService,
-    IConnectionStore connectionStore,
-    IRealTimeMessageNotifier messageNotifier
+    IRealTimeMessageNotifier messageNotifier,
+    ICacheService cacheService,
+    IJsonHelper<Chat> jsonHelper
 ) : IRequestHandler<UpdateMessageCommand>
 {
   public async Task Handle(UpdateMessageCommand command, CancellationToken cancellationToken)
   {
     var messageId = Guid.Parse(command.MessageId);
+
     var message = await dbContext.Messages
         .Where(m => m.Id == messageId && m.SenderId == currentUserService.UserId)
         .Include(c => c.Sender)
@@ -31,23 +33,38 @@ public class UpdateMessageHandler(
     message.IsEdited = true;
     await dbContext.SaveChangesAsync(cancellationToken);
 
-    var chat = await dbContext.Chats
-                .Where(c => c.Id == message.ChatId)
-                .Include(c => c.ChatMembers)
-                .FirstOrDefaultAsync(cancellationToken);
+    Chat? chat;
+
+    string key = $"chat {message.ChatId}";
+
+    var cachedChat = await cacheService.GetValueAsync(key);
+
+    if (cachedChat != null)
+    {
+      chat = jsonHelper.Decode(cachedChat);
+    }
+    else
+    {
+      chat = await dbContext.Chats
+        .AsNoTracking()
+        .Where(c => c.Id == message.ChatId)
+        .Select(c => new Chat
+        {
+          Id = c.Id,
+          ChatMembers = c.ChatMembers.Select(cm => new ChatMember
+          {
+            UserId = cm.UserId
+          }).ToList()
+        })
+        .FirstOrDefaultAsync(cancellationToken);
+
+      await cacheService.SetValueAsync(key, jsonHelper.Encode(chat), TimeSpan.FromMinutes(30));
+    }
+
 
     var recieverId = chat?.ChatMembers?.FirstOrDefault(m => m.UserId != currentUserService.UserId)?.UserId;
 
-    if (recieverId != null &&
-        recieverId != Guid.Empty &&
-        connectionStore.TryGet(recieverId.ToString(), out string recieverConnectionId))
-    {
-      await messageNotifier.SendToUserAsync(recieverConnectionId, "MessageUpdate", message.ToDto());
-    }
-
-    if (connectionStore.TryGet(currentUserService.UserId.ToString(), out string senderConnectionId))
-    {
-      await messageNotifier.SendToUserAsync(senderConnectionId, "UpdateDone", message.ToDto());
-    }
+    await messageNotifier.SendToUserAsync(recieverId.ToString(), "MessageUpdate", message.ToDto());
+    await messageNotifier.SendToUserAsync(currentUserService.UserId.ToString(), "UpdateDone", message.ToDto());
   }
 }
